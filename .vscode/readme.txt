c6d817731b7c7c1e085198f0c1969fc20491485d
DESKTOP-VM8VPA1\shanky <shah2nkhan@live.com>
2018-08-12 23:12:36 +0530

    FirstCut for offline support

---
+++ .vscode/readme.txt
@@ -0,0 +1,14 @@
+
+npm start
+// To set up package manager
+npm install --save-dev electron
+
+npm install --save-dev electron-packager
+npm run package-win
+
+http://www.iconarchive.com/show/windows-8-icons-by-icons8/Healthcare-Suicide-Risk-icon.html
+
+https://ourcodeworld.com/articles/read/524/how-to-use-live-reload-in-your-electron-project
+
+https://www.christianengvall.se/electron-packager-tutorial/
+
---
+++ main.js
@@ -0,0 +1,109 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const { app, BrowserWindow, Menu, ipcMain } = electron;
+
+
+process.env.NODE_ENV = 'production';
+let mainWindow, instSearchWindow;
+
+app.on('ready', function () {
+    mainWindow = new BrowserWindow({});
+    mainWindow.loadURL(url.format({ pathname: path.join(__dirname, 'mainWindow.html'), protocol: 'file', slashes: true }));
+
+    mainWindow.on('closed', function () {
+        app.quit();
+    });
+
+    const mainMenu = Menu.buildFromTemplate(mainMenuBarTemplate);
+    Menu.setApplicationMenu(mainMenu);
+
+});
+
+
+//Quit App when Main window Close
+
+
+
+function LaunchSearchPopup() {
+    instSearchWindow = new BrowserWindow({
+        title: 'Bond Risk',
+        height: 200,
+        width: 300,
+        frame: false
+    });
+
+
+
+    instSearchWindow.loadURL(url.format({ pathname: path.join(__dirname, 'searchInstWindow.html'), protocol: 'file', slashes: true }));
+
+    const childmenu = Menu.buildFromTemplate(emptyMenuBarTemplate);
+    instSearchWindow.setMenu(childmenu);
+
+    instSearchWindow.on('close', function () {
+        instSearchWindow = null;
+    });
+
+}
+
+//Handling ipc Message
+
+ipcMain.on('Inst:search', function (e, item) {
+    console.log(item);
+    mainWindow.webContents.send('Inst:search', item);
+    instSearchWindow.close();
+});
+
+
+
+const mainMenuBarTemplate = [
+    {
+        label: 'Views',
+        submenu:
+            [
+                {
+                    label: 'Search instrument',
+                    click() { LaunchSearchPopup(); }
+                },
+                {
+                    label: 'Clear',
+                    click() {
+                        mainWindow.webContents.send('Inst:clear');
+
+                    }
+                },
+                {
+                    label: 'Quit', accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+                    click() { app.quit(); }
+                }
+            ]
+    }
+];
+
+const emptyMenuBarTemplate = [];
+
+// Add blank menu if MAC
+if (process.platform == 'darwin') {
+    mainMenuBarTemplate.unshift({});
+}
+
+//Add developer tools if not in production
+
+if (process.env.NODE_ENV !== 'production') {
+    mainMenuBarTemplate.push({
+        label: 'Developers tools',
+        submenu: [
+            {
+                label: 'Toogle Devtools',
+                accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+                click(item, focusedWindow) {
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+                role: 'reload'
+            }
+        ]
+    });
+}
